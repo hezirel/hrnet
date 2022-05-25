63f7cc99a16042cd7ed1c5e1d36d90a8daf88fb2
hezirel <80560775+hezirel@users.noreply.github.com>
2022-05-25 17:38:53 +0200

    Base setup store

---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+import { 
+    combineReducers
+} from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({
+    //name: name.reducer => Imported from "features/name/nameSlice"
+});
+
+
+export default rootReducer;
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,7 +2,8 @@ import {
   configureStore
 } from '@reduxjs/toolkit';
 
+import rootReducer from '../reducers/rootReducer';
+
 export const store = configureStore({
-  reducer: {
-  },
+  reducer: rootReducer,
 });
