07e5ff6a5969b0f891df9376c1c8834124f46f67
hezirel <80560775+hezirel@users.noreply.github.com>
2022-06-01 12:44:07 +0200

    Working datepick date reset on submit

--- src/components/app/entry/modules/datepick/Datepick.jsx
+++ src/components/app/entry/modules/datepick/Datepick.jsx
@@ -1,6 +1,5 @@
 import {
 	React,
-	useState,
 } from "react";
 
 import { 
@@ -17,15 +16,12 @@ const Datepick = ({ field = "dob"}) => {
 		field === "dob" ? 
 			faker.date.birthdate : faker.date.future
 	)) => start().toISOString().substring(0, 10);
-	const [startDate, setStartDate] = useState(baseDate);
-	startDate ? true : false;
 
 	return (
 		<input
 			type="date"
 			name={field}
 			defaultValue={baseDate()}
-			onChange={(e) => setStartDate(new Date(e.target.value))}
 			required
 		>
 		</input>
